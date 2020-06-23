2ad0a37ddf5f22258bb81307bae9561ecd3aa52d
OlgaGatalskaya <gatalolia86@gmail.com>
2020-06-24 01:44:44 +0200

    add pages and refactoring of code

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ErrorIndicator from '../error-indicator/error-indicator';
-import ItemDetails, {Record} from '../item-details/item-details';
-import Row from '../row';
 import SwapiService from '../../services/swapi-service';
+import {SwapiServiceProvider} from '../swapi-service-context';
+import ErrorBoundry from '../error-boundry';
+import {PeoplePage, PlanetsPage, StarshipsPage} from '../pages'
 import './app.css';
+
 
 
 
@@ -17,8 +19,6 @@ export default class App extends Component  {
         hasError: false
     }
 
-   
-
     componentDidCatch() {
         this.setState({
             hasError: true
@@ -26,41 +26,24 @@ export default class App extends Component  {
     }
 
     render() {
-
         if(this.state.hasError) {
             return <ErrorIndicator />
         }
 
-        const {getPerson, getStarship, getPersonImage, getStarshipImage} = this.swapiService
-
-        const personDetails = (
-            <ItemDetails itemId={11}
-                getData={getPerson}
-                getImageUrl={getPersonImage}>
-                <Record field='gender' label='Gender'/>
-                <Record field='eyeColor' label='Eye Color'/>
-            </ItemDetails>
-        );
-        const starshipDetails = (
-            <ItemDetails itemId={5}
-                getData={getStarship}
-                getImageUrl={getStarshipImage}>
-                <Record field='model' label='Model'/>
-                <Record field='crew' label='Crew'/>
-                <Record field='costInCredits' label='Cost'/>
-            </ItemDetails>
-        )
 
         return (
-            <div className='app'>
-                <Header />
-                <RandomPlanet />
+            <ErrorBoundry>
+                <SwapiServiceProvider value = {this.swapiService}>
+                    <div className='app'>
+                        <Header />
+                        <RandomPlanet />
 
-                {/*<PeoplePage />*/}
-                <Row
-                    left={personDetails}
-                    right={starshipDetails} />    
-            </div>
+                        <PeoplePage/>
+                        <PlanetsPage/>
+                        <StarshipsPage/>    
+                    </div>
+                </SwapiServiceProvider>
+            </ErrorBoundry>
         )
     }
 
